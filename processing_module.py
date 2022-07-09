e5171a8f41f4899b24b838a8c0e44dd5e27a3106
jannis <j.langerpascual@campus.tu-berlin.de>
2022-07-09 14:52:29 +0200

    added features + UI changes 09.07

--- processing_module.py
+++ processing_module.py
@@ -1,9 +1,10 @@
 import sys
 import numpy as np
+import cv2
+import matplotlib.pyplot as plt
+
 #from PyQt5.QtWidgets import QApplication, QMainWindow, QSlider, QLabel, QPushButton, QLineEdit
 #from PyQt5.QtCore import Qt
-import cv2
-#from Interface import ParticleUserinterface
 PATH = r'Results/gui_img.jpg'  # Path jpg File
 DPI = 100
 SCALE = 0.5
@@ -14,7 +15,7 @@ class ParticleProcessing():
     def __init__(self, path):
         # init parameters
         self.text_input = False
-        self.bgThresh = 128
+        self.bgThresh = 200
         self.peaksThresh = 0.45
         self.blockSize = 41
         self.C = 3
@@ -46,17 +47,22 @@ class ParticleProcessing():
     def grayBlur(self):
         return cv2.GaussianBlur(cv2.cvtColor(self.img, cv2.COLOR_RGB2GRAY), (self.blur1, self.blur1), 0)
 
-    def bg_thresh(self, thresh=None):
-        if thresh:
+    def bg_thresh(self, thresh=None, contours = False):
+        if thresh is not None:
             self.bgThresh = thresh
         th, bw = cv2.threshold(self.gray, self.bgThresh, 255, cv2.THRESH_BINARY_INV)
         self.bgMask = cv2.morphologyEx(bw, cv2.MORPH_OPEN, self.kernel5)
-        res = self.bgMask
-        #cv2.imshow('res', res)
+
+        if contours:
+            res = self.img.copy()
+            contours, hierarchy = cv2.findContours(self.bgMask, cv2.RETR_CCOMP, cv2.CHAIN_APPROX_SIMPLE)
+            cv2.drawContours(res, contours, -1, (0, 255, 0), 2)
+        else:
+            res = self.bgMask
         return res
 
-    def ad_thresh(self, block = None, c = None):
-        if block:
+    def ad_thresh(self, block=None, c=None, contours = False):
+        if block is not None:
             self.blockSize = block
         if c:
             self.C = c
@@ -66,23 +72,37 @@ class ParticleProcessing():
         #print(self.BGimg.shape)
         masked = cv2.bitwise_and(ad_th, ad_th, mask=self.bgMask)
         self.ad_out = cv2.morphologyEx(masked, cv2.MORPH_OPEN, self.kernel3)
-        res = self.ad_out
+
+        if contours:
+            res = self.img.copy()
+            contours, hierarchy = cv2.findContours(self.ad_out, cv2.RETR_CCOMP, cv2.CHAIN_APPROX_SIMPLE)
+            cv2.drawContours(res, contours, -1, (0, 255, 0), 2)
+        else:
+            res = self.ad_out
         return res
 
-    def p_thresh(self, thresh=None):
-        if thresh:
+    def p_thresh(self, thresh=None, contours = False):
+        if thresh is not None:
             self.peaksThresh = thresh
-        dist = cv2.distanceTransform(self.ad_out, cv2.DIST_L2, cv2.DIST_MASK_PRECISE)
+
+        self.dist = cv2.distanceTransform(self.ad_out, cv2.DIST_L2, cv2.DIST_MASK_PRECISE)
         borderSize = int((self.particle_size / 2) + self.gap)
-        distborder = cv2.copyMakeBorder(dist, borderSize, borderSize, borderSize, borderSize,
+        distborder = cv2.copyMakeBorder(self.dist, borderSize, borderSize, borderSize, borderSize,
                                         cv2.BORDER_CONSTANT | cv2.BORDER_ISOLATED, 0)
         self.nxcor = cv2.matchTemplate(distborder, self.distTempl, cv2.TM_CCOEFF_NORMED)
         # Peaks
         mn, mx, _, _ = cv2.minMaxLoc(self.nxcor)
         th, peaks = cv2.threshold(self.nxcor, mx * self.peaksThresh, 255, cv2.THRESH_BINARY)
+
+
         peaks8u = cv2.convertScaleAbs(peaks)
         self.peaks8u = cv2.convertScaleAbs(peaks)
-        res = self.peaks8u
+        if contours:
+            res = self.img.copy()
+            contours, hierarchy = cv2.findContours(self.peaks8u, cv2.RETR_CCOMP, cv2.CHAIN_APPROX_SIMPLE)
+            cv2.drawContours(res, contours, -1, (0, 255, 0), 2)
+        else:
+            res = self.peaks8u
         return res
 
     def hi(self, x):
@@ -90,7 +110,53 @@ class ParticleProcessing():
 
     def process_all(self):
         self.bg_thresh()
+        self.ad_thresh()
+        res = self.p_thresh()
+        return res
 
+    def calcParticles(self):
+        self.process_all()
+        self.img_draw = self.img.copy()
+        contours, hierarchy = cv2.findContours(self.peaks8u, cv2.RETR_CCOMP, cv2.CHAIN_APPROX_SIMPLE)
+        counter = 0
+        size = np.zeros(len(contours))
+        for i in range(len(contours)):
+            # cv2.drawContours(self.img_draw, contours, i, (0, 0, 255), 1)
+            x, y, w, h = cv2.boundingRect(contours[i])
+            _, mx, _, mxloc = cv2.minMaxLoc(self.dist[y:y + h, x:x + w])
+            if 7.5 <= mx <= 400:
+                center = (int(mxloc[0] + x), int(mxloc[1] + y))
+                radius = int(np.round(mx))
+                cv2.circle(self.img_draw, center, radius, (255, 0, 0), 1)
+                size[counter] = mx * 2
+                counter += 1
+
+        size = np.trim_zeros(size)
+        mean = np.sum(size) / counter
+        cv2.putText(self.img_draw, "counter: " + str(int(counter)), (75, 50),
+                    cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
+        cv2.putText(self.img_draw,
+                    "av. size: : " + str(round(mean, 2)),
+                    (75, 80), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
+
+        #res = resizeImg(self.img_draw, SCALE)
+        cv2.imshow('img', self.img_draw)
+        ### Histogramm
+        size.tofile('Files/sample.csv', sep='\n')
+        file = open("Files/sample.csv")
+        numpy_array = np.array(size)
+        n, bins, patches = plt.hist(x=numpy_array, bins='auto', color='#0504aa',
+                                    alpha=0.7, rwidth=0.85)
+
+        plt.grid(axis='y', alpha=0.75)
+        plt.xlabel('Size')
+        plt.ylabel('Frequency')
+        plt.title('Histogram')
+        #plt.text(23, 45, r'$\mu=15, b=3$')
+        maxfreq = n.max()
+        # Set a clean upper y-axis limit.
+        plt.ylim(ymax=np.ceil(maxfreq / 10) * 10 if maxfreq % 10 else maxfreq + 10)
+        plt.show()
 
     def stackImages(self, scale, imgArray):
         rows = len(imgArray)
@@ -131,6 +197,8 @@ class ParticleProcessing():
 
 if __name__ == "__main__":
     a = ParticleProcessing(PATH)
-    a.ad_thresh()
+    a.calcParticles()
+    #res = a.calcParticles()
+    #cv2.imshow('eres', res)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
